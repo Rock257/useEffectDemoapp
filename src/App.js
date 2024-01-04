be7f076bf92a7b8132ec90c877d0684f34b48cb1
CodeSandbox <support@codesandbox.io>
2024-01-04 10:20:18 +0000

    Sandbox snapshot commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./styles.css";
+import DemoApp from "./component/DemoApp";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello Learners</h1>
+      <h2>Laearn useEffect</h2>
+      <h3>
+        Definataion :-{" "}
+        <pre>
+          he useEffect Hook allows you to perform side effects in your
+          components.
+        </pre>{" "}
+      </h3>
+      <h3>
+        Synatax <pre>useEffect(funtion, dependencies?)</pre>{" "}
+      </h3>
+      <div className="compo">
+        <DemoApp />
+      </div>
     </div>
   );
 }
